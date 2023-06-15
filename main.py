c20e2976a5c8d86b83331667250c6b325652a94c
phiniki <14hiroto1610@gmail.com>
2023-06-15 12:40:29 +0900

    commit

--- main.py
+++ main.py
@@ -6,12 +6,34 @@ import pandas as pd
 
 app = Flask(__name__)
 
+page_dr = {
+    "LoopContent":{"name":"繰り返し表示", "html":"LoopContent.html"},
+    "ReadExcel_And_Show":{"name":"エクセル読み込み＆表示", "c":"ReadExcel_And_Show.html"},
+}
+
 @app.route('/', methods = ["GET"])
 def index():
-    df = pd.read_excel("input/ReadExcel_And_Show.xlsx")
-    data = df.to_dict(orient='records')
 
-    response = make_response(render_template('index.html', data=data))
+    response = make_response(render_template('index.html', page_dr=page_dr))
+    return response
+
+@app.route('/<string:page>/', methods = ["GET"])
+def page(page):
+    if page == "LoopContent":
+        data = [
+            {"title":"aa", "content":"aaaaaaaaaaaaaa"},
+            {"title":"bb", "content":"bbbbbbbbbbbbbb"},
+        ]
+        response = make_response(render_template(page_dr[page]["html"], data=data))
+        return response
+    
+    elif page == "ReadExcel_And_Show":
+        df = pd.read_excel("input/ReadExcel_And_Show.xlsx")
+        data = df.to_dict(orient='records')
+        response = make_response(render_template(page_dr[page]["html"], data=data))
+        return response
+
+    response = make_response(render_template(page_dr[page]["html"], data=data))
     return response
 
 app.run(host='localhost', port=8888, debug=True)
